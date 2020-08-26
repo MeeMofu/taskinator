1b4e22b5071fa942e53454c1bc9289ec541c4fcc
VTran <MeeMofu@users.noreply.github.com>
2020-08-26 07:44:32 -0400

    Added button

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var buttonEl = document.querySelector("#save-task");
+// console.log(buttonEl);
+// buttonEl.addEventListener("click", function() {
+//     alert("button clicked");
+//   });
+var tasksToDoEl=document.querySelector("#tasks-to-do");
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+}
+buttonEl.addEventListener("click",createTaskHandler);
